9020fd291d11a302821d789395202f25d72902c3
samat <somych1@gmail.com>
2022-11-20 00:42:28 -0600

    navbar redesigned

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +9,14 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  // If { static: true }
   ngOnInit(): void {
   }
+  
+  status: boolean = false;
+  myFunction() {
+    this.status = !this.status;
+  }
+
 
 }
